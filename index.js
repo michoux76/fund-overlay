8a9b0904cd0e729a7c5ab791fbc4547c4a1ac06f
michoux76 <michoux7654@gmail.com>
2022-10-02 22:58:20 +0200

    ok

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const request = require('request')
+
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -7,4 +9,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/count',(req,res)=>request('https://gateway.gofundme.com/web-gateway/v1/feed/matt-waldens-hurricane-ian-relief-fund/counts').pipe(res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
